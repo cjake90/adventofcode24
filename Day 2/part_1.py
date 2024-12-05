29bbe0b291ea60b4fca6849b50c2aee6a7974c02
cjake90 <45885503+cjake90@users.noreply.github.com>
2024-12-06 00:03:29 +0100

    Updated part 1 solution

--- Day 2/part_1.py
+++ Day 2/part_1.py
@@ -2,6 +2,16 @@ import numpy as np
 
 solution = 0
 
+def check_if_safe(arr):
+    dir = np.sign(arr[0])
+    for x in diff:
+        if 0==dir or np.sign(x) != dir:
+            return False
+        elif 3 < abs(x) or 0 == x:
+            return False
+    return True
+
+
 with open("input.txt") as file:
     for line in file:
         str_arr = line.split()
@@ -10,17 +20,6 @@ with open("input.txt") as file:
 
         diff = np.diff(arr)
         
-        dir = np.sign(diff[0])
-
-
-        skip = False
-        for x in diff:
-            if 0==dir or np.sign(x) != dir:
-                skip = True
-                break
-            elif 3 < abs(x) or 0 == x:
-                skip = True
-                break
-        if False == skip:
+        if check_if_safe(diff):
             solution+=1
 print(solution)
